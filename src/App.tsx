12c1ac7de52116afbdf91575231ba84b4d30e13c
Jennifer Goijman <jennifer@journi.uk>
2023-11-20 16:33:04 +0100

    feat: add Personal Information section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { PersonalInformation } from './components/PersonalInformation/PersonalInformation'
 
 function App() {
 
   return (
     <>
       <h1>Jennifer Goijman</h1>
+      <PersonalInformation />
     </>
   )
 }
---
+++ src/components/PersonalInformation/PersonalInformation.tsx
@@ -0,0 +1,21 @@
+export const PersonalInformation = () => {
+  return (
+    <div style={{ marginBottom: 32 }}>
+      <p style={{ fontSize: 30, marginTop: 8  }}>
+        Full Stack Developer
+      </p>
+      <p style={{ fontSize: 18, marginTop: 8 }}>
+        <span style={{ marginRight: 16 }}>📍</span>
+        Madrid, Spain
+      </p>
+      <p style={{ fontSize: 18, marginTop: 8 }}>
+        <span style={{ marginRight: 16 }}>📫</span>
+        jgoijman@gmail.com
+      </p>
+      <p style={{ fontSize: 18, marginTop: 8 }}>
+        <span style={{ marginRight: 16 }}>📞</span>
+        +34644943349
+      </p>
+    </div>
+  )
+}
