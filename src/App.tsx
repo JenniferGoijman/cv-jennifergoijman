9a69c156fd76b38f2a25624bac35fc4c8bd84cf4
Jennifer Goijman <jennifer@journi.uk>
2023-11-27 14:45:16 +0100

    feat: add Work Experience section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { PersonalInformation } from './components/PersonalInformation/PersonalIn
 import { useTranslation } from 'react-i18next'
 import { Section } from './components/common/Section/Section'
 import { Languages } from './components/Languages/Languages'
+import { Jobs } from './components/Jobs/Jobs'
 
 
 function App() {
@@ -22,6 +23,18 @@ function App() {
 
       <Section title="About Me">
         <p>{t('aboutMe.text')}</p>
+      </Section>
+
+      <Section title="Work Experience">
+        <Jobs />
+        {/*
+        <JobSection company="Journi" position="Frontend Engineer" dates="March 2023 - Present">
+          <SkillsContainer skills={['react', 'typescript', 'swr', 'mobx', 'styledComponents', 'cypress', 'jest', 'storybook', 'vite', 'git', 'figma']} />
+        </JobSection>
+        <JobSection company="One Beyond" position="Full Stack Developer" dates="August 2020 - March 2023" >
+          <SkillsContainer skills={['react', 'javascript', 'redux', 'styledComponents', 'mui', 'node', 'postgresql', 'cypress', 'jest', 'webpack', 'git', 'figma']} />
+        </JobSection>
+        */}
       </Section>
 
       <Section title="Languages">
---
+++ src/components/Jobs/Jobs.tsx
@@ -0,0 +1,33 @@
+import { useTranslation } from "react-i18next";
+import { Paragraph } from "../common/Typography/Typography";
+
+const jobsArray = ['journi', 'oneBeyond'];
+
+const CompanyName = ({ text }: { text: string }) => <Paragraph size="20">{text}</Paragraph>
+const JobPosition = ({ text }: { text: string }) => <p style={{ fontSize: 16 }}>{text}</p>
+const Dates = ({ text }: { text: string }) => <Paragraph size="16">{text}</Paragraph>
+
+export const Jobs = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      {jobsArray.map(job => {
+        const descriptions: string[] = t(`jobs.${job}.description`, {  returnObjects: true })
+
+        return (
+        <div style={{ marginBottom: 20 }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: 16 }}>
+            <div style={{ textAlign: 'left' }}>
+              <CompanyName text={t(`jobs.${job}.name`)} />
+              <JobPosition text={t(`jobs.${job}.position`)} />
+            </div>
+            <Dates text={t(`jobs.${job}.dates`)} />
+          </div>
+
+          {descriptions.map(description => <Paragraph>{description}</Paragraph>)}
+        </div>
+      )})}
+    </>
+  )
+}
