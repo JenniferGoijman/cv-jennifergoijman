bc7c55f416a9d83c76acf852c403fadfc4488dec
Jennifer Goijman <jennifer@journi.uk>
2023-11-27 17:09:15 +0100

    fix: assets base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cv-jennifergoijman/',
+  optimizeDeps: {
+    include: [
+      'i18next',
+      'react',
+      'react-dom',
+      'react-i18next',
+      'uniqid',
+    ]
+  }
 })
