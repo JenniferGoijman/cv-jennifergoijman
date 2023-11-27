2bae69c7715a5694a929e4e38d906993e606fa4e
Jennifer Goijman <jennifer@journi.uk>
2023-11-27 10:37:07 +0100

    chore: install i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 import { PersonalInformation } from './components/PersonalInformation/PersonalInformation'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+  const { i18n } = useTranslation();
+
+  const language = i18n.language;
 
   return (
     <>
+      <button onClick={() => i18n.changeLanguage(language === 'en' ? 'es' : 'en')}>
+        {language === 'en' ? 'Versión en Español' : 'English Version'}
+      </button>
+
       <h1>Jennifer Goijman</h1>
+
       <PersonalInformation />
     </>
   )
--- src/components/PersonalInformation/PersonalInformation.tsx
+++ src/components/PersonalInformation/PersonalInformation.tsx
@@ -1,15 +1,30 @@
+import { useTranslation } from "react-i18next";
 import { TextWithIcon } from "../common/Typography/Typography"
 import { Container, SubHeader } from "./PersonalInformation.styled"
 
 export const PersonalInformation = () => {
+  const { t } = useTranslation();
+
   return (
     <Container>
       <SubHeader>
         Full Stack Developer
       </SubHeader>
-      <TextWithIcon icon="📍" text="Madrid, Spain" />
-      <TextWithIcon icon="📫" text="jgoijman@gmail.com" />
-      <TextWithIcon icon="📞" text="+34644943349" />
+
+      <TextWithIcon
+        icon={t('personalInformation.location.icon')}
+        text={t('personalInformation.location.text')}
+        />
+
+      <TextWithIcon
+        icon={t('personalInformation.email.icon')}
+        text={t('personalInformation.email.text')}
+        />
+
+      <TextWithIcon
+        icon={t('personalInformation.phone.icon')}
+        text={t('personalInformation.phone.text')}
+        />
     </Container>
   )
 }
